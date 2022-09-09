a906c88bbf6484185fd416bc36180f056f7deb74
jovi848 <j.mcintyre@senit.com>
2022-09-08 21:12:27 -0400

    initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textStyle}>Jovi Chen-McIntyre</Text>
+      <Text style={styles.textStyle}>301125059</Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyle:{
+    fontSize: 20,
+  }
+});
